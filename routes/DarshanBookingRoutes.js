6e27946293786a9f80a410d02af100ac005b5568
Sandeep123karan <sandeepkaran234@gmail.com>
2026-02-16 18:00:59 +0530

    Refactor DarshanBookingRoutes to use new controller methods

--- routes/DarshanBookingRoutes.js
+++ routes/DarshanBookingRoutes.js
@@ -1,16 +1,22 @@
+
 const express = require("express");
-const {
-  getAllDarshanBookings,
-  createDarshanBooking,
-  updateDarshanBooking,
-  deleteDarshanBooking
-} = require("../controllers/DarshanBookingController");
-
 const router = express.Router();
+const controller = require("../controllers/darshanBookingController");
 
-router.get("/", getAllDarshanBookings);
-router.post("/", createDarshanBooking);
-router.put("/:id", updateDarshanBooking);
-router.delete("/:id", deleteDarshanBooking);
+// CREATE booking
+router.post("/", controller.createDarshanBooking);
+
+// GET all bookings (admin)
+router.get("/", controller.getDarshanBookings);
+
+// GET single booking
+router.get("/:id", controller.getSingleBooking);
+
+// UPDATE booking
+router.put("/:id", controller.updateBooking);
+
+// DELETE booking
+router.delete("/:id", controller.deleteBooking);
 
 module.exports = router;
+
